ada00d46b1348f0ae041d511485fa6cba1f04af8
Cezar Barroso Pinto Junior <cezarjr347@gmail.com>
2024-04-19 12:08:26 -0300

    modulo
    codigo modularizado
    criar arquivo palavras
    criar arquivo ranking

--- script.py
+++ script.py
@@ -1,33 +1,18 @@
 import random
-from flask import Flask, render_template, request, redirect, session
+import metodos as mtd
+from flask import Flask, render_template, request, session
 
 app = Flask(__name__)
 app.secret_key = "serve"
 
+mtd.criar_arquivo_palavras()
+
+
 @app.route("/")
 def index():
-# Tente abrir o arquivo no modo de leitura
-    try:
-        with open("ranking.txt", "r") as arquivo:
-            grupo_ranking = []
-            for linha in arquivo:
-                palavra = linha.strip()
-                grupo_ranking.append(palavra)
-        grupo_ranking.sort(key=lambda x: int(x.split(';')[4]), reverse=True)
-    except FileNotFoundError:
-        with open("ranking.txt","a+") as arquivo:
-            grupo_ranking = []
-            for linha in arquivo:
-                palavra = linha.strip()
-                grupo_ranking.append(palavra)
-        grupo_ranking.sort(key=lambda x: int(x.split(';')[4]), reverse=True)
-
-    for i, palavra in enumerate(grupo_ranking, 1):
-        palavra_split = palavra.split(';')
-        palavra_split.insert(0, str(i))
-        grupo_ranking[i - 1] = ';'.join(palavra_split)
-    
+    grupo_ranking = mtd.ler_criar_ranking()    
     return render_template("index.html", titulo="Jogo",  grupo_ranking=grupo_ranking)
+
 
 @app.route("/iniciar", methods=["POST"])
 def escolher_grupo():
@@ -35,10 +20,12 @@ def escolher_grupo():
     grupo_escolhido = request.form.get("grupo")
     dificuldade_escolhida = request.form.get("dificuldade")
     jogador = request.form.get("nome").strip()
+
     while "  " in jogador:
         jogador = jogador.replace("  ", " ")
-    palavra_secreta = carrega_palavra_secreta(grupo_escolhido)
-    letras_acertadas = inicializa_letras_acertadas(palavra_secreta)
+
+    palavra_secreta = mtd.carrega_palavra_secreta(grupo_escolhido)
+    letras_acertadas = mtd.inicializa_letras_acertadas(palavra_secreta)
     session["palavra_secreta"] = palavra_secreta
     session["letras_acertadas"] = letras_acertadas
     session["grupo_escolhido"] = grupo_escolhido
@@ -46,7 +33,6 @@ def escolher_grupo():
     session["jogador"] = jogador
     erros = 0
     session["erros"] = erros
-
   
     return render_template("Start.html", grupo_escolhido=grupo_escolhido
                                         , dificuldade_escolhida=dificuldade_escolhida
@@ -58,6 +44,8 @@ def escolher_grupo():
                                         , jogador=jogador
                                         , letra = chute
                             )
+
+
 @app.route("/jogar", methods=["POST"])
 def jogar():
     chute = request.form.get("letra")
@@ -69,23 +57,26 @@ def jogar():
     grupo_escolhido = session.get("grupo_escolhido", None)
     dificuldade_escolhida = session.get("dificuldade_escolhida", None)
     jogador = session.get("jogador", None)
-    max_erros = nivel_dificuldade(dificuldade_escolhida)
+    max_erros = mtd.nivel_dificuldade(dificuldade_escolhida)
     
     if(chute in palavra_secreta):
-        marca_chute_correto(chute, letras_acertadas, palavra_secreta)
+        mtd.marca_chute_correto(chute, letras_acertadas, palavra_secreta)
     else:
         erros += 1
         session["erros"] = erros
+
     if session["erros"] >= max_erros:
         enforcou = True
-        pontos = calcular_pontos(len(palavra_secreta), "DERROTA", erros, max_erros)
-        popular_ranking(jogador,dificuldade_escolhida,"DERROTA", palavra_secreta, pontos)
-        return render_template("Fim.html", resultado="DERROTA", jogador=jogador) #CARREGAR POPUP COM A MENSAGEM DERROTA (PONTOS, NOME JOGADOR, COLOCACAO)
+        pontos = mtd.calcular_pontos(len(palavra_secreta), "DERROTA", erros, max_erros)
+        mtd.popular_ranking(jogador,dificuldade_escolhida,"DERROTA", palavra_secreta, pontos)
+        return render_template("Fim.html", resultado="DERROTA", jogador=jogador) 
+    
     if "_" not in letras_acertadas:
         acertou = True
-        pontos = calcular_pontos(len(palavra_secreta), "VITORIA", erros, max_erros)
-        popular_ranking(jogador,dificuldade_escolhida,"VITORIA", palavra_secreta, pontos)
-        return render_template("Fim.html", resultado="VITORIA", jogador=jogador, disparar_confetes=True) #CARREGAR POPUP COM A MENSAGEM VITORIA (PONTOS, NOME JOGADOR, COLOCACAO)
+        pontos = mtd.calcular_pontos(len(palavra_secreta), "VITORIA", erros, max_erros)
+        mtd.popular_ranking(jogador,dificuldade_escolhida,"VITORIA", palavra_secreta, pontos)
+        return render_template("Fim.html", resultado="VITORIA", jogador=jogador, disparar_confetes=True)
+    
     return render_template("Start.html", titulo="Jogo Forca"
                                         , grupo_escolhido=grupo_escolhido
                                         , dificuldade_escolhida=dificuldade_escolhida
@@ -99,62 +90,7 @@ def jogar():
                                         , max_erros = max_erros
                                         , jogador=jogador
                                         )
-def popular_ranking(nome, nivel, status, palavra, pontuacao):
-    with open("ranking.txt", "a") as arquivo:
-        arquivo.write(f"{nome};{nivel};{status};{palavra};{pontuacao}\n")
 
-def calcular_pontos(tamanho_palavra, resultado_jogo, letras_erradas, nivel_dificuldade):
-    fator_tamanho_palavra = 5
-    fator_letras_erradas = -5
-    fator_nivel_dificuldade = 5
-
-    pontos_tamanho_palavra = tamanho_palavra * fator_tamanho_palavra
-    pontos_resultado = 100 if resultado_jogo == "VITORIA" else 0 
-    pontos_letras_erradas = letras_erradas * fator_letras_erradas
-    pontos_nivel_dificuldade = (10 - nivel_dificuldade) * fator_nivel_dificuldade  
-
-    pontos_totais = pontos_tamanho_palavra + pontos_resultado + pontos_letras_erradas + pontos_nivel_dificuldade
-    return pontos_totais
-
-
-def carrega_palavra_secreta(grupo_escolhido): #passar o grupo escolhido
-    arquivo = open("palavras.txt","r")
-    palavras_grupo = []
-
-    for linha in arquivo:
-        linha = linha.strip()
-        coluna = linha.split(";")
-        if coluna[0].strip().upper() == grupo_escolhido.upper():
-            palavras_grupo.append(coluna[1].strip().upper())
-    arquivo.close()
-
-    numero = random.randrange(0, len(palavras_grupo))
-    palavra_secreta = palavras_grupo[numero].upper()
-    return palavra_secreta
-
-
-def marca_chute_correto(chute, letras_acertadas, palavra_secreta):
-    index = 0
-    for letra in palavra_secreta:
-        if (chute == letra):
-            letras_acertadas[index] = letra
-        index += 1
-    # Atualiza a lista de letras acertadas na sessão
-    session["letras_acertadas"] = letras_acertadas
-
-
-def inicializa_letras_acertadas(palavra): # retorna o numero de caracteres da palavra
-    return ["_" for letra in palavra]
-
-
-def nivel_dificuldade(dificuldade):
-    if dificuldade == "FACIL":
-        max_erros = 10
-    elif dificuldade == "MEDIO":
-        max_erros = 5
-    elif dificuldade == "DIFICIL":
-        max_erros = 2
-    return max_erros
 
 if __name__ == "__main__":
     app.run(debug=True)
